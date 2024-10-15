09086d9d5b7c7564960150b8a347e798a9ef48aa
naveed ahmed <naveedahmed2@yahoo.com>
2024-10-16 01:55:20 +0500

    first commit

---
+++ myfirstproject/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { Main } from "next/document";
+import { styleText } from "util";
+
+export default function home(){
+  return(
+    
+
+    
+      
+      <div> <h1> ghazala aijaz  </h1></div>
+    
+    
+  )
+
+
+     
+}
